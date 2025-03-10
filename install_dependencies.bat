841b93987d89b3f7dedfc742e938b83eec293ebd
Andre <Andre@domain.com>
2025-03-10 00:43:03 -0300

    added conan pkg manager

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -1,8 +1,16 @@
 @echo off
+setlocal enabledelayedexpansion
 
-echo Installing Python dependencies (Conan pkg manager) ...
+echo Installing Conan package manager ...
 pip install -r requirements.txt
 
 echo Installing Conan dependencies ...
 conan install . --build=missing
 
+echo Adding Conan to system PATH env ...
+call .\conanbuild.bat 
+setx PATH "!PATH!"
+
+echo Checking for ninja and cmake ...
+ninja --version
+cmake --version
